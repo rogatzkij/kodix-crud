1bf7301f5e74b0eca7f149976e6f816de69d508b
Андрей Рогацкий <rogatzkij@yandex.ru>
2020-08-26 17:08:10 +0300

    добавлены настройки

--- config/config.go
+++ config/config.go
@@ -1,1 +1,20 @@
 package config
+
+import "github.com/kelseyhightower/envconfig"
+
+// Настройки сервиса
+type Config struct {
+	Port  int    `default:"8080"`                      // порт, на котором будет запущен сервис
+	Mongo string `default:"mongodb://127.0.0.1:27017"` // адрес монги
+}
+
+// Считываем настройки из переменных окружения
+func GetConfigFromEnv() (*Config, error) {
+	conf := &Config{}
+	err := envconfig.Process("KODIX", conf)
+	if err != nil {
+		return nil, err
+	}
+
+	return conf, nil
+}
